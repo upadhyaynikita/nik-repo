e5812e28193f5bef345325a3f7eff7309464de9c
Nikita Upadhyay <nikita.upadhyay@infocepts.com>
2023-09-18 09:48:42 +0000

    second before test commit

---
+++ macros/warn_if_null_with_insert.sql
@@ -0,0 +1,36 @@
+{% test warn_if_null_with_insert(model,column_name) %}
+
+    {{ config(severity = 'warn') }}
+
+{% set null_check_query %}
+        
+    select 
+        '{{ target.database }}' ,
+        '{{ target.schema }}',
+        '{{ model }}',
+        '90',
+        'IS_NULL_TEST',
+        'Null in key column -> {{ column_name }} in {{ model }}',
+        current_timestamp(),
+        'BSNS_ENTTY_CODE_ABC',
+        '{{ file_name }}',
+        value,
+        current_timestamp()
+        from {{model}}
+        where ({{column_name}}  is NULL)
+
+{% endset %}
+{{ null_check_query }}
+
+
+{% if execute %}
+    --{{ insert_reject_records(null_check_query) }}
+    {% set result_val = insert_reject_records(null_check_query) %}      
+{% endif %}
+
+
+      
+
+{% endtest %}
+
+
